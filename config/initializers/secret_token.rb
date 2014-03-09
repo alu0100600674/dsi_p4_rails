35110ad5fd0a27f098627f85871b46badf8246e3
Jonay Suárez Armas <alu0100600674@ull.edu.es>
2014-03-09 20:19:44 +0000

    Proyecto inicializado

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DsiP4Rails::Application.config.secret_key_base = 'da984dc577bf8ed0035946f59f21d1adbe03005fe09c6afb96e2395ae9248b398688553196c6fe09b135897fd80162c27764cb56543cb87a6c6eabad8d264cf7'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+DsiP4Rails::Application.config.session_store :cookie_store, key: '_dsi_p4_rails_session'
